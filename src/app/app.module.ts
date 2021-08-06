a9a6f2b717c3eca65259f19c26c87f40579ac7a2
Siddharth <siddharth.muscat@gmail.com>
2021-08-06 13:03:05 +0530

    Add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,22 @@ import { EditorComponent } from './components/editor/editor.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EditorComponent,
-    LayoutComponent,
-    NavbarComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		EditorComponent,
+		LayoutComponent,
+		NavbarComponent,
+		FooterComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
