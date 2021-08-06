39669ba086b4b00faf48e80504c4120eff2704e6
Siddharth <siddharth.muscat@gmail.com>
2021-08-06 14:48:59 +0530

    Make markdown-it generate heading anchors

--- src/app/services/markdown-parser.service.ts
+++ src/app/services/markdown-parser.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import * as MarkdownIt from 'markdown-it';
+import * as MarkdownItAnchor from 'markdown-it-anchor';
 
 @Injectable({
 	providedIn: 'root'
@@ -9,7 +10,7 @@ export class MarkdownParserService {
 		html: true,
 		linkify: true,
 		typographer: true
-	});
+	}).use(MarkdownItAnchor);
 
 	parseMarkdown(markdown: string): string {
 		return this.md.render(markdown)
