7c9cdd45280d94d81f52f6c45acc0b78170291e9
Siddharth <siddharth.muscat@gmail.com>
2021-08-09 14:11:58 +0530

    Make file names work

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,20 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SafeHtml} from '@angular/platform-browser';
 import {MarkdownParserService} from 'src/app/services/markdown-parser.service';
+import {StorageService} from 'src/app/services/storage.service';
 
 @Component({
 	selector: 'app-editor',
 	templateUrl: './editor.component.html',
 	styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent {
-
+export class EditorComponent implements OnInit {
 	formInput: string = '';
+	currentFile: string = localStorage.getItem('latestFileEdit') || 'undefined.md';
+	newFileName: string = this.currentFile;
 
-	constructor(private markdownParser: MarkdownParserService) {}
+	constructor(private markdownParser: MarkdownParserService, private storageService: StorageService) {}
 
 	parseMD(markdown: string): SafeHtml {
-		return this.markdownParser.parseMarkdown(markdown)
+		return this.markdownParser.parseMarkdown(markdown);
 	}
 
+	ngOnInit(): void {
+		if (!Object.keys(this.storageService._get()).length) this.storageService.writeFile('untitled.md', 'Hey there!');
+		if (!localStorage.getItem('latestFileEdit')) localStorage.setItem('latestFileEdit', 'untitled.md')
+		this.loadFile();
+	}
+
+	writeFile() {
+		this.storageService.writeFile(this.currentFile, this.formInput);
+	}
+
+	loadFile() {
+		this.formInput = this.storageService.getFiles()[this.currentFile] || '';
+	}
+
+	updateFileName() {
+		this.storageService.removeFile(this.currentFile);
+		this.storageService.writeFile(this.newFileName, this.formInput);
+		this.currentFile = this.newFileName;
+		localStorage.setItem('latestFileEdit', this.currentFile)
+	}
+
+	getFileList() {
+		return Object.keys(this.storageService.getFiles())
+	}
 }
