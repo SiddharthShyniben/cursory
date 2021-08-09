1d39deb90da9ddc946a7f067b0cbd13adc088e5a
Siddharth <siddharth.muscat@gmail.com>
2021-08-09 11:33:21 +0530

    Make navbar toggle work

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+	navOpen: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	toggleNav() {
+		this.navOpen = !this.navOpen;
+	}
 }
