569035e892c4c795711daf3d872a2d91d100d76d
Siddharth <siddharth.muscat@gmail.com>
2021-08-09 14:47:19 +0530

    Set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EditorComponent} from './components/editor/editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '/', component: EditorComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
